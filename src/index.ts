fe5f0487c6002e9ce1e7e85945adbb1586fc3a4b
Marcus Kjell <marcus.kjell@gmail.com>
2019-01-29 23:56:03 +0100

    register resolver

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,26 @@
-import { ApolloServer } from 'apollo-server-express';
-import * as Express from 'express';
-import 'reflect-metadata';
-import { buildSchema, Query, Resolver } from 'type-graphql';
+import { ApolloServer } from 'apollo-server-express'
+import * as Express from 'express'
+import 'reflect-metadata'
+import { buildSchema } from 'type-graphql'
+import { createConnection } from 'typeorm'
+
+import { RegisterResolver } from './modules/user/Register'
 
 const main = async () => {
-
-  @Resolver()
-  class HelloResolver {
-    @Query(() => String)
-    async hello() {
-      return await 'Hello World!'
-    }
-  }
-
+  await createConnection()
   const schema = await buildSchema({
-    resolvers: [HelloResolver],
-  });
-  
-  const apolloServer = new ApolloServer({schema})
+    resolvers: [RegisterResolver],
+  })
+
+  const apolloServer = new ApolloServer({ schema })
 
   const app = Express()
 
-  apolloServer.applyMiddleware({app})
+  apolloServer.applyMiddleware({ app })
 
   app.listen(4000, () => {
     console.log('server started on http://localhost:4000/graphql')
   })
-
 }
 
 main()
