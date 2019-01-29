61effc3624b63636088c444cd24ca13584555276
Marcus Kjell <marcus.kjell@gmail.com>
2019-01-30 00:23:38 +0100

    validation

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,28 +1,30 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
-import { ObjectType, Field, ID } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm'
+import { ObjectType, Field, ID, Root } from 'type-graphql'
 
 @ObjectType()
 @Entity()
 export class User extends BaseEntity {
   @Field(() => ID)
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number
 
   @Field()
   @Column()
-  firstName: string;
+  firstName: string
 
   @Field()
   @Column()
-  lastName: string;
+  lastName: string
 
   @Field()
-  @Column("text", { unique: true })
-  email: string;
+  @Column('text', { unique: true })
+  email: string
 
   @Field()
-  name: string;
+  name(@Root() parent: User): string {
+    return `${parent.firstName} ${parent.lastName}`
+  }
 
   @Column()
-  password: string;
+  password: string
 }
